6509fb51fb566bc4a154ff454f26acbcd1ce036a
Srishti8107 <srishtiumesha@gmail.com>
2026-06-16 23:30:48 +0530

    Basic url shortener created

---
+++ script.js
@@ -0,0 +1,39 @@
+const shortbtn = document.getElementById("shortbtn");
+const shortUrl = document.getElementById("short_url");
+async function createShortUrl(longUrl) {
+    const apiKey = "ey72pgYhlTtKRKsd26SquBL9QbkCSxKoNn2uvii5sDKm9KAtDvgrdAiPqllw";
+    const website = "https://api.tinyurl.com/create";
+
+    const info = {
+        url : longUrl,
+        domain : 'tinyurl.com'
+    };
+
+    try {
+        const response = await fetch(website, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        });
+        const result = await response.json();
+        if(response.ok){
+            shortUrl.textContent = result.data.tiny_url;
+            // console.log('Custom Short URL: ', result.data.tiny_url);
+            // return result.data.tiny_url;
+        }else {
+            console.error('Error from API: ', result.errors);
+        }
+
+    } catch (error) {
+        console.error('Network error:', error);
+    }
+}
+shortbtn.addEventListener("click",() => {
+    const inputElement = document.getElementById("long_url");
+    const longUrl = inputElement.value;
+    createShortUrl(longUrl);
+});
+
