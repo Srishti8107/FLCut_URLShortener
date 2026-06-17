aacb6a85199ada170fedcbb28b5f1c32c49fdfc1
Srishti8107 <srishtiumesha@gmail.com>
2026-06-17 22:18:56 +0530

    Dashboard table updated according to localStorage of the browser

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ import {CONFIG} from './config.js';
 const shortbtn = document.getElementById("shortbtn");
 const shortUrl = document.getElementById("short_url");
 const msgContainer = document.getElementById("msgContainer");
+const dashboardTable = document.getElementById("dashboardTable");
 
 // error message div creation
 const errmsg = document.createElement('div');
 errmsg.className = ("errmsg");
 
 let resultUrl;
-let linkData={};
+// loading links from localstorage at the begining
+let links = JSON.parse(localStorage.getItem("shortlinks")) || [];
+displaylinks();
 
 async function createShortUrl(longUrl, customWord) {
     const apiKey = `${CONFIG.apiKey}`;
@@ -40,8 +43,27 @@ async function createShortUrl(longUrl, customWord) {
 
         if(response.ok){
             shortUrl.textContent = result.data.tiny_url;
-            return resultUrl = await result.data.tiny_url;
+            resultUrl = result.data.tiny_url;
+            if (resultUrl){
+                const linkData = {
+                    longUrl,
+                    resultUrl
+                }
+
+                links.push(linkData);
+                localStorage.setItem("shortlinks", JSON.stringify(links));
+                displaylinks();
+            }
             //console.log('Custom Short URL: ', result.data.tiny_url);
+
+            // Copy to clipboard option by just clicking on text
+            const copybtn = document.getElementById("copybtn");
+            copybtn.addEventListener("click", async () => {
+                await navigator.clipboard.writeText(resultUrl);
+                console.log(resultUrl);
+                copybtn.textContent = "Copied!";
+            });
+
         }else {
             errmsg.innerText = result.errors;
             msgContainer.appendChild(errmsg);
@@ -49,6 +71,7 @@ async function createShortUrl(longUrl, customWord) {
             setTimeout(() => {
                 errmsg.remove();
             }, 3000);
+            return null;
         }
 
     } catch (error) {
@@ -57,29 +80,42 @@ async function createShortUrl(longUrl, customWord) {
         setTimeout(() => {
                 errmsg.remove();
             }, 3000);
+            return null;
         //console.error('Network error:', error);
     }
 
 }
 
+
+
 shortbtn.addEventListener("click",() => {
     const longUrl = document.getElementById("long_url").value;
     const customWord = document.getElementById("customWord").value.trim();
     resultUrl = createShortUrl(longUrl, customWord);
-    linkData={
-        longUrl:longUrl,
-        shortUrl: resultUrl
-    };
-    console.log(linkData)
+    console.log(resultUrl);
+    
 });
 
-// Copy to clipboard option by just clicking on text
-const copybtn = document.getElementById("copybtn");
-copybtn.addEventListener("click", async () => {
-    await navigator.clipboard.writeText(resultUrl);
-    console.log(resultUrl);
-    copybtn.textContent = "Copied!";
-});
+//display links function
+function displaylinks(){
+    links.forEach(link => {
+        dashboardTable.innerHTML += `
+            <tr>
+                <td>
+                    <a href="${link.longUrl}" target="_blank">
+                        ${link.longUrl}
+                    </a>
+                </td>
+                <td>
+                    <a href="${link.resultUrl}" target="_blank">
+                        ${link.resultUrl}
+                    </a>
+                </td>
+            </tr>
+        `;
+    });
+}
+
 
 
 
