1276a56ea68ca704ca68c04093780a60dbf69f9c
Srishti8107 <srishtiumesha@gmail.com>
2026-06-17 18:56:08 +0530

    Website almost done with alias included --> drawback : alias is not used unless it is unique and does not exist in tinyurl databse

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+import {CONFIG} from './config.js';
 const shortbtn = document.getElementById("shortbtn");
 const shortUrl = document.getElementById("short_url");
-let resultUrl = "";
-async function createShortUrl(longUrl) {
-    const apiKey = "ey72pgYhlTtKRKsd26SquBL9QbkCSxKoNn2uvii5sDKm9KAtDvgrdAiPqllw";
-    const website = "https://api.tinyurl.com/create";
+let resultUrl;
+let linkData={};
 
+async function createShortUrl(longUrl, customWord) {
+    const apiKey = `${CONFIG.apiKey}`;
+    // console.log(apiKey);
+    const website = `${CONFIG.website}`;
+    
     const info = {
         url : longUrl,
         domain : 'tinyurl.com'
     };
+    
+    if (customWord !== ""){
+        info.alias = customWord;
+        //console.log(info);
+    }
 
     try {
         const response = await fetch(website, {
@@ -17,14 +26,16 @@ async function createShortUrl(longUrl) {
                 'Authorization': `Bearer ${apiKey}`,
                 'Content-Type': 'application/json'
             },
+
             body: JSON.stringify(info)
         });
+
         const result = await response.json();
+
         if(response.ok){
             shortUrl.textContent = result.data.tiny_url;
-            return resultUrl= result.data.tiny_url;
-            // console.log('Custom Short URL: ', result.data.tiny_url);
-            
+            return resultUrl = await result.data.tiny_url;
+            //console.log('Custom Short URL: ', result.data.tiny_url);
         }else {
             console.error('Error from API: ', result.errors);
         }
@@ -32,16 +43,22 @@ async function createShortUrl(longUrl) {
     } catch (error) {
         console.error('Network error:', error);
     }
+
 }
+
 shortbtn.addEventListener("click",() => {
-    const inputElement = document.getElementById("long_url");
-    const longUrl = inputElement.value;
-    createShortUrl(longUrl);
+    const longUrl = document.getElementById("long_url").value;
+    const customWord = document.getElementById("customWord").value.trim();
+    resultUrl = createShortUrl(longUrl, customWord);
+    linkData={
+        longUrl:longUrl,
+        shortUrl: resultUrl
+    };
+    console.log(linkData)
 });
 
 // Copy to clipboard option by just clicking on text
 const copybtn = document.getElementById("copybtn");
-
 copybtn.addEventListener("click", async () => {
     await navigator.clipboard.writeText(resultUrl);
     console.log(resultUrl);
